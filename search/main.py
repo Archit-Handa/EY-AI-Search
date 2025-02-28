d8f2ecaeebfd834eacf61442bab53d59e1d1c301
Archit-Handa <archit20handa@gmail.com>
2025-02-28 16:36:45 +0530

    Modularized code main.py works as the API

--- search/main.py
+++ search/main.py
@@ -1,27 +1,19 @@
-import streamlit as st
-import requests
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pdf_loader import PDFLoader
 
-def main():
-    st.title('AI Search - EY')
-    st.write('Upload a Document (PDF, DOCX, JSON, or TXT file)')
+app = Flask(__name__)
+CORS(app, origins='*')
+
+@app.route('/extract-text', methods=['POST'])
+def extract_text():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
     
-    uploaded_file = st.file_uploader('Upload your file', type=['pdf', 'docx', 'json', 'txt'])
+    pdf_file = request.files['file']
+    extracted_text = PDFLoader.load_pdf(pdf_file)
     
-    if uploaded_file is not None:
-        st.write(f'### Uploaded File: {uploaded_file.name}')
-    
-    if uploaded_file is not None:
-        st.title("PDF Text Extractor")
-        st.write("Upload a PDF file and extract text using the API.")
-        if st.button("Extract Text"):
-            files = {"file": uploaded_file.getvalue()}
-            response = requests.post("http://127.0.0.1:5000/extract-text", files=files)
-            
-            if response.status_code == 200:
-                extracted_text = response.json().get("extracted_text", "No text extracted")
-                st.text_area("Extracted Text:", extracted_text, height=300)
-            else:
-                st.error("Failed to extract text.")
+    return jsonify({'extracted_text': extracted_text}), 200
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
