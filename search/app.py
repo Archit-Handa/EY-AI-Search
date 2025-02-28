8f6f5ead8227a1831688fc7e66d37749ba84cc6b
Archit-Handa <archit20handa@gmail.com>
2025-02-28 17:11:24 +0530

    Aesthetic frontend changes

--- search/app.py
+++ search/app.py
@@ -8,20 +8,19 @@ def main():
     uploaded_file = st.file_uploader('Upload your file', type=['pdf', 'docx', 'json', 'txt'])
     
     if uploaded_file is not None:
-        st.write(f'### Uploaded File: {uploaded_file.name}')
-    
-    if uploaded_file is not None:
-        st.title('PDF Text Extractor')
-        st.write('Upload a PDF file and extract text using the API.')
         if st.button('Extract Text'):
-            files = {'file': uploaded_file.getvalue()}
-            response = requests.post('http://127.0.0.1:5000/extract-text', files=files)
-            
-            if response.status_code == 200:
-                extracted_text = response.json().get('extracted_text', 'No text extracted')
-                st.text_area('Extracted Text:', extracted_text, height=300)
-            else:
-                st.error('Failed to extract text.')
+            with st.status('Extracting Text...', expanded=True) as status:
+                files = {'file': uploaded_file.getvalue()}
+                response = requests.post('http://127.0.0.1:5000/extract-text', files=files)
+                
+                if response.status_code == 200:
+                    status.update(label='Text Extracted', state='complete')
+                    extracted_text = response.json().get('extracted_text', 'No text extracted')
+                    st.text_area('Extracted Text:', extracted_text, height=300)
+                    
+                else:
+                    status.update(label='Failed to extract text', state='error')
+                    st.error('Failed to extract text.')
 
 if __name__ == '__main__':
     main()
