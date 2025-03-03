c2d84ac565095be9476f02407d4a9e9b3cbf8147
Archit-Handa <archit20handa@gmail.com>
2025-03-03 16:13:18 +0530

    Added docstrings

--- search/frontend/document_fetchers/document_uploader.py
+++ search/frontend/document_fetchers/document_uploader.py
@@ -2,6 +2,8 @@ import streamlit as st
 from .base import DocumentFetcher
 
 class DocumentUploader(DocumentFetcher):
+    '''Fetch and return the document uploaded by the user'''
+    
     def fetch_document(self):
         st.write('Upload a Document (PDF, DOCX, JSON, or TXT file)')
         return st.file_uploader('Upload your file', type=['pdf', 'docx', 'json', 'txt'])
