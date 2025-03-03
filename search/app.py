269703b07619ddda4ae92dc5de8ae558127db14e
Archit-Handa <archit20handa@gmail.com>
2025-03-03 14:40:25 +0530

    Added functionality to fetch docs from Azure Blob Storage

--- search/app.py
+++ search/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 from azure.storage.blob import BlobServiceClient
+from io import BytesIO
 
 BACKEND_URL_PATH = 'http://127.0.0.1:5000'
 
@@ -8,12 +9,13 @@ def upload_file():
     st.write('Upload a Document (PDF, DOCX, JSON, or TXT file)')
     return st.file_uploader('Upload your file', type=['pdf', 'docx', 'json', 'txt'])
 
-def fetch_file():
+def fetch_azure_file():
     AZURE_CONNECTION_STRING = st.text_input(
-            label='Azure Storage Connection String',
-            value=None,
-            placeholder='Enter your Azure storage connection string'
-        )
+        label='Azure Storage Connection String',
+        value=None,
+        placeholder='Enter your Azure storage connection string',
+        type='password'
+    )
     CONTAINER_NAME = st.text_input(
         label='Container Name',
         value=None,
@@ -25,15 +27,21 @@ def fetch_file():
         container_client = blob_service_client.get_container_client(CONTAINER_NAME)
         
         blob_list = [blob.name for blob in container_client.list_blobs()]
-        return st.selectbox("Choose a file from Azure Blob Storage:", blob_list)
+        filename = st.selectbox("Choose a file from Azure Blob Storage:", blob_list)
+        
+        blob_client = container_client.get_blob_client(filename)
+        blob_stream = BytesIO(blob_client.download_blob().readall())
+        
+        return filename, blob_stream
     
     return None
 
-def extract_file(file):
+def extract_file(file, fetched=False):
     extract_button = st.empty()
     if extract_button.button('Extract Text'):
         with st.status('Extracting Text...', expanded=True) as status:
-            files = {'file': (file.name, file.getvalue())}
+            files = {'file': (file.name, file.getvalue())} if not fetched \
+                else {'file': (file[0], file[1])}
             response = requests.post(f'{BACKEND_URL_PATH}/extract-text', files=files)
             
             if response.status_code == 200:
@@ -51,11 +59,12 @@ def main():
     st.title('AI Search - EY')
     
     option = st.radio('Choose how to upload file:', ('Upload a file', 'Fetch from Azure'))
-    if option == 'Upload a file': file = upload_file()
-    elif option == 'Fetch from Azure': file = fetch_file()
-    else: file = None
             
-    if file is not None: extract_file(file)
+    file, fetched = (upload_file(), False) if option == 'Upload a file' \
+        else (fetch_azure_file(), True) if option == 'Fetch from Azure' \
+            else (None, None)
+            
+    if file is not None: extract_file(file, fetched)
 
 if __name__ == '__main__':
     main()
