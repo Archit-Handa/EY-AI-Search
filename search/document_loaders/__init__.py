8112e313d568a094b4c2a9ce5a24763b839ecb4f
Archit-Handa <archit20handa@gmail.com>
2025-03-03 11:28:26 +0530

    Fixed bug

--- search/document_loaders/__init__.py
+++ search/document_loaders/__init__.py
@@ -1,17 +1,17 @@
 from .base import DocumentLoader
 from .pdf_loader import PDFLoader
 from .docx_loader import DocxLoader
-from .txt_loader import TextLoader
+from .txt_loader import TxtLoader
 from .json_loader import JsonLoader
 
 def get_loader(file_extension: str) -> DocumentLoader:
     loaders = {
         'pdf': PDFLoader(),
         'docx': DocxLoader(),
-        'txt': TextLoader(),
+        'txt': TxtLoader(),
         'json': JsonLoader()
     }
     
     return loaders.get(file_extension.lower(), None)
 
-__all__ = ['PDFLoader', 'DocxLoader', 'TextLoader', 'JsonLoader', 'get_loader']
+__all__ = ['PDFLoader', 'DocxLoader', 'TxtLoader', 'JsonLoader', 'get_loader']
