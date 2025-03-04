483d62693df88660d2cdd148e926b0fa07ec0280
Archit-Handa <archit20handa@gmail.com>
2025-03-04 15:21:50 +0530

    Adapted paragraph chunker to consider single line breaks as delimiters

--- search/backend/chunkers/paragraph_chunker.py
+++ search/backend/chunkers/paragraph_chunker.py
@@ -5,5 +5,5 @@ class ParagraphChunker(Chunker):
     '''Chunk a text into smaller chunks based on paragraph breaks'''
     
     def chunk(self, text: str) -> Generator[str, None, None]:
-        for para in text.split('\n\n'):
+        for para in text.split('\n'):
             yield para
