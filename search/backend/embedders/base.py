2a78090d8144632fe7cb67907a64c119b42adb1c
Archit-Handa <archit20handa@gmail.com>
2025-03-04 16:34:52 +0530

    Added base embedder

---
+++ search/backend/embedders/base.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+class Embedder(ABC):
+    '''Base class for embedders'''
+    
+    def __call__(self, input: Union[str, list[str]]) -> Union[list[float], list[list[float]]]:
+        '''
+        Wrapper function to embed a text or a list of texts when the model is called
+        
+        @param input: The text or list of texts to embed
+        @return: The embeddings
+        '''
+        if not input:
+            return []
+        
+        if isinstance(input, str):
+            return self.__embed([input])[0]
+        elif isinstance(input, list) and all(isinstance(item, str) for item in input):
+            return self.__embed(input)
+        else:
+            raise ValueError('Input must be a string or a list of strings')
+    
+    @abstractmethod
+    def _embed(self, input: list[str]) -> list[list[float]]:
+        '''
+        Embed a list of texts
+        
+        @param input: The list of texts to embed
+        @return: The embeddings
+        '''
+        pass
