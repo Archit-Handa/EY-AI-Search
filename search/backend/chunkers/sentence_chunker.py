ab6dfcca1993fa73ffd7e168e0a8404e5f1d97ad
Archit-Handa <archit20handa@gmail.com>
2025-03-04 15:34:16 +0530

    Improved the sentence chunker to split based on .,!,?

--- search/backend/chunkers/sentence_chunker.py
+++ search/backend/chunkers/sentence_chunker.py
@@ -1,9 +1,10 @@
 from .base import Chunker
 from typing import Generator
+import re
 
 class SentenceChunker(Chunker):
-    '''Chunk a text into smaller chunks based on sentence breaks '.' (full stops)'''
+    '''Chunk a text into smaller chunks based on sentence breaks - period (.), exclamation mark (!), question mark (?)'''
     
     def chunk(self, text: str) -> Generator[str, None, None]:
-        for sent in text.split('.'):
-            yield sent
+        for sent in re.split(r'([.!?])\s*', text):
+            if sent not in ['', '.', '!', '?']: yield sent
