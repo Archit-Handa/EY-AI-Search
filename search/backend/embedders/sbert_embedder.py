7c4b927250b83e644c966539b7a47233a45ddc05
Archit-Handa <archit20handa@gmail.com>
2025-03-04 16:35:09 +0530

    Added SBERT Embeddert

---
+++ search/backend/embedders/sbert_embedder.py
@@ -0,0 +1,14 @@
+from .base import Embedder
+from sentence_transformers import SentenceTransformer
+
+class SBertEmbedder(Embedder):
+    def __init__(self, model_name: str='all-MiniLM-L6-v2'):
+        self.model = SentenceTransformer(model_name)
+    
+    def _embed(self, input: list[str]) -> list[list[float]]:
+        try:
+            return self.model.encode(input, convert_to_numpy=False, normalize_embeddings=True).tolist()
+        
+        except Exception as e:
+            print(f'Error embedding text: {e}')
+            return []
