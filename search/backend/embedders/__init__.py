f77ef06470cc18a4256ded70d23ee3c7c582ec7b
Archit-Handa <archit20handa@gmail.com>
2025-03-17 12:50:11 +0530

    Added docstring for get_embedder

--- search/backend/embedders/__init__.py
+++ search/backend/embedders/__init__.py
@@ -8,6 +8,12 @@ _EMBEDDERS = {
 }
 
 def get_embedder(embedder_type: str, **kwargs) -> Embedder:
+    '''
+    Retrieve the embedder for embedding the text
+    
+    @param embedder_type: Type of embedder
+    @return: Embedder for embedding
+    '''
     embedder_class = _EMBEDDERS.get(embedder_type.lower())
     if embedder_class is None:
         raise ValueError(f'Unknown embedder: {embedder_type}')
